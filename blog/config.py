2c39a36b55f9ec20e60cade6d13dcb9716fed99a
dennellmarie <dennell.lucero@gmail.com>
2016-12-23 12:21:14 -0800

    updated config with db url

--- blog/config.py
+++ blog/config.py
@@ -6,7 +6,7 @@
 
 import os
 class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://dennell:thinkful@localhost:5432/blogful"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://dennell:thinkful@localhost:5432/blogful")
     DEBUG = True
     SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", os.urandom(12))
 
